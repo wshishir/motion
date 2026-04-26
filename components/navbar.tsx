cbf6f0cb1649297a91857a7d47b0fdea45744814
Shishir <shishirtech506@gmail.com>
2026-04-27 05:04:49 +0530

    add:navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,28 @@
-"use client"
-import React from 'react'
+"use client";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import React from "react";
+import Logo from "./logo";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
+  const scrolled = useScrollTop();
   return (
-    <div></div>
-  )
-}
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-xs",
+      )}
+    >
+      <Logo />
+      <div
+        className="md:ml-auto md:justify-end justify-between w-full flex
+      items-center gap-x-2"
+      >
+        <Button className="cursor-pointer active:scale-90">Login</Button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
