a7f9436699158dce73c1daff4a27b26c5923bf8d
Shishir <shishirtech506@gmail.com>
2026-05-03 03:31:39 +0530

    stuff

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,16 +1,56 @@
 "use client";
-import React from "react";
+import { cn } from "@/lib/utils";
+import { ChevronsLeft } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 const Navigation = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const pathname = usePathname();
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<HTMLElement>(null);
+  const navbarRef = useRef<HTMLDivElement>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
     <>
-      <aside className=" group/sidebar h-full flex bg-secondary overflow-y-auto relative w-60 z-99999 flex-col">
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full flex bg-secondary overflow-y-auto relative w-60 z-99999 flex-col",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0",
+        )}
+      >
+        <div
+          role="button"
+          className={cn(
+            "h-6 w-6 text-muted-foreground absolute top-2 right-2 opacity-0 group-hover/sidebar:opacity-100 transition cursor-pointer",
+            isMobile && "opacity-100",
+          )}
+        >
+          <ChevronsLeft className="h-6  w-6" />
+        </div>
         <div>
           <p>Action Items</p>
         </div>
         <div className="mt-4">Documents</div>
-        <div className="opacity-0 group-hover/sidebar:opacity-100" />
+        <div
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize
+        absolute h-full bg-primary/10 right-0 top-0 w-1"
+        />
       </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 z-99999 left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full",
+        )}
+      ></div>
     </>
   );
 };
